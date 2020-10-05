af4881e9fc4218fec85b23efbd885f28d21d8327
Arjun Srivastava <arjunbazinga@gmail.com>
2020-10-06 00:47:41 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # speak
-google tts based select to speak 
+
+google tts based select to speak
+
+# install
+
+1. get dependencies
+2. change path to gcp credentials in `speak.sh`
+3. add keyboard shortcut
+
+# dependencies
+
+Install [google cloud sdk](https://cloud.google.com/sdk/docs/install)
+and enable [tts api](https://cloud.google.com/text-to-speech)
+
+`sudo apt install xsel jq mpg123 curl`
+
+- xsel to get selected text
+- jq to create json
+- mpg123 to play mp3 audio
+- curl to send request
--- speak.sh
+++ speak.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-  # "voice":{
-  #   "languageCode":"en-gb",
-  #   "name":"en-GB-Standard-A",
-  #   "ssmlGender":"FEMALE"
-  # },
-
-
 # find a better way to kill last command
 killall mpg123
 
+
+# adding gcloud to path
 source /home/rjn/Documents/experiments/google-cloud-sdk/env.sh
 
 export GOOGLE_APPLICATION_CREDENTIALS="/home/rjn/Documents/Michael_Burry/config/secrets/Michael Burry -c4c263169f45.json"
@@ -33,5 +28,4 @@ jq -n --rawfile text file.txt '{
 -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
 -H "Content-Type: application/json; charset=utf-8" \
 -d@- \
-https://texttospeech.googleapis.com/v1/text:synthesize | tac | tac \
-| jq ".audioContent" -r|   base64 --decode | mpg123 -
+https://texttospeech.googleapis.com/v1/text:synthesize | jq ".audioContent" -r|  base64 --decode | mpg123 -
